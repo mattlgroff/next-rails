3469516cadf8b8495b537bc2b2edd2e82a23070a
Matt Groff <matt@umbrage.com>
2023-06-08 22:21:27 -0400

    Fix some build errors in the templates

--- generate/code/generateNewPage.js
+++ generate/code/generateNewPage.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 function generateNewPage(singularModelName, pluralModelName) {
   const modelName = singularModelName.charAt(0).toUpperCase() + singularModelName.slice(1);
-  const PascalSingleModelName = singularModelName.charAt(0).toUpperCase() + singularModelName.slice(1);
+  const PascalSingularModelName = singularModelName.charAt(0).toUpperCase() + singularModelName.slice(1);
   const PascalPluralModelName = pluralModelName.charAt(0).toUpperCase() + pluralModelName.slice(1);
 
   // Use a Promise to make the function asynchronous
   return new Promise((resolve, reject) => {
     ejs.renderFile(
       path.resolve(__dirname, './templates/newPage.ejs'),
-      { singularModelName, pluralModelName, modelName, PascalPluralModelName, PascalSingleModelName },
+      { singularModelName, pluralModelName, modelName, PascalPluralModelName, PascalSingularModelName },
       {},
       function (err, str) {
         if (err) reject(err);
